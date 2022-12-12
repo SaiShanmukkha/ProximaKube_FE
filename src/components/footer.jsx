6b78df06f581f32411efddecf6d6a3bd5f711364
saishanmukkha <saishanmukkha@gmail.com>
2022-12-13 00:48:47 +0530

    Nav&Foot Bar Design1

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,12 +2,42 @@ import styles from "../../styles/Footer.module.css"
 
 
 export default function Footer() {
+  const year = "2022";
   return (
-    <div className={styles.footerWrapper}>
-        <hr/>
-        <div className={styles.footerContent}>
-            <span className={styles.mainTitle}>SSS Proxima</span>
-            <span>@2022 All rights reserved.</span>
+    <div className={`${styles.footerWrapper} ${styles.noselect}`}>
+        <div className={styles.footerBlock}>
+
+          <div className={styles.footerTitleCard}>
+            <div className={styles.logoSection}>
+              <img src={'/logo.png'} className={styles.logo} width={'45px'} height={'45px'}/>
+              <div className={styles.LogoTitle}>ProximaKube</div>
+            </div>
+            <div className={styles.titleContent}>
+              <p>"The beautiful thing about learning is that nobody can take it away from you."</p>
+            </div>
+          </div>
+
+          <div className={styles.footerNavigCard}>
+            <div className="NavigLinks">
+              <h2>Important Links</h2>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className={styles.footerSubscribeCard}>
+            <div className="Subscribe">
+              <h2>Get Notification</h2>
+              <input type="text" />
+              <button>subscribe</button>
+            </div>
+            <div className="copyRight">
+              <span>@{year} ProximaKube. All rights reserved.</span>
+            </div>
+          </div>
         </div>
     </div>
   );
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,6 @@
 import styles from "../../styles/NavBar.module.css";
+import { AiOutlineSearch } from "react-icons/ai";
+import { MdOutlineClear } from "react-icons/md";
 
 export default function NavBar({router}) {
   const onclickAction = () => {
@@ -10,8 +12,29 @@ export default function NavBar({router}) {
 
   return (
     <div className={styles.NavBar} onClick={onclickAction}>
-      <img src={'/logo.png'} className={styles.logo} width={'45px'} height={'45px'}/>
-      <div className={styles.LogoTitle}>ProximaKube</div>
+      <div className={styles.NavBarBlock}>
+        <div className={styles.logoSection}>
+          <img src={'/logo.png'} className={styles.logo} width={'45px'} height={'45px'}/>
+          <div className={styles.LogoTitle}>ProximaKube</div>
+        </div>
+
+        <div className={styles.searchBarBlock}>
+          <div className={styles.searchBar}>
+            <AiOutlineSearch className={styles.searchIcon} />
+            <input className={`${styles.searchInput}`} type="text" placeholder="Search here" />
+            <MdOutlineClear />
+          </div>
+        </div>
+
+        <div className={`${styles.NavMenu} ${styles.noselect}`}>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+
+      </div>
     </div>
   );
 }
